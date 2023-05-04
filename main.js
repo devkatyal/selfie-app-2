b2cb554c409a868925ad8df0170919b7f356c0d4
devkatyal <74549043+devkatyal@users.noreply.github.com>
2023-05-04 18:49:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+Webcam.set({
+    width: 320,
+   height: 240,
+   dest_width: 320,
+   dest_height: 240,
+   image_format: 'jpeg',
+   jpeg_quality: 90,
+  });
+
+  
+  Webcam.attach( '#my_camera' );
